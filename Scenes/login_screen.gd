a186d4a2ca643d3233ebdac2c18d1836c9b24284
JaspherCustodio <27jasphercustodio@gmail.com>
2024-10-24 14:24:22 +0800

    Init

---
+++ Scenes/login_screen.gd
@@ -0,0 +1,5 @@
+extends Node
+
+
+func _on_signup_button_pressed() -> void:
+	SceneSwitcher.switch_scene("res://Scenes/signup_screen.tscn")
---
+++ Scenes/signup_screen.gd
@@ -0,0 +1,5 @@
+extends Node
+
+
+func _on_login_button_pressed() -> void:
+	SceneSwitcher.switch_scene("res://Scenes/login_screen.tscn")
