10d92b38196ea5924c91f4d5eac6ba5ab326f751
JaspherCustodio <27jasphercustodio@gmail.com>
2024-11-06 18:47:20 +0800

    nice

--- Scenes/intro_scene.gd
+++ Scenes/intro_scene.gd
@@ -2,5 +2,5 @@ extends Control
 
 
 func _ready() -> void:
-	await (get_tree().create_timer(3).timeout)
-	queue_free()
+	await (get_tree().create_timer(5).timeout)
+	SceneSwitcher.switch_scene("res://Scenes/login_screen.tscn")
